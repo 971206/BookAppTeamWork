8ce6e0471f851bfae9bf4881bdc4512fee13390b
Mariami93 <marijoglidze@gmail.com>
2021-07-08 18:28:11 +0400

    create DetailsViewController

--- BookAppTeamWork/BookAppTeamWork/Infrastructure/Coordinator/AppCoordinator.swift
+++ BookAppTeamWork/BookAppTeamWork/Infrastructure/Coordinator/AppCoordinator.swift
@@ -24,7 +24,7 @@ final class AppCoordinator: CoordinatorProtocol {
     }
     
     func start() {
-        let vc = MainViewController.instantiateFromStoryboard()
+        let vc = DetailsViewController.instantiateFromStoryboard()
         vc.coordinator = self
         navigationController?.pushViewController(vc, animated: true)
         
---
+++ BookAppTeamWork/BookAppTeamWork/Presentation/Scenes/Details/View/DetailsViewController.swift
@@ -0,0 +1,21 @@
+//
+//  DetailsViewController.swift
+//  BookAppTeamWork
+//
+//  Created by AltaSoftware MacMINI on 7/8/21.
+//
+
+import UIKit
+
+class DetailsViewController: BaseViewController {
+
+    @IBOutlet weak var elipseView: UIView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+    
+  
+
+}
